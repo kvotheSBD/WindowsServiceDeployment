c0be5c901a4ff1e250d694513cf61ff364cf7889
erodriguez <erodriguez.lazaro@atsistemas.com>
2019-07-09 12:37:50 +0200

    Modified csproj file

--- DotNetCoreApp/obj/Debug/netcoreapp2.2/DotNetCoreApp.RazorTargetAssemblyInfo.cs
+++ DotNetCoreApp/obj/Debug/netcoreapp2.2/DotNetCoreApp.RazorTargetAssemblyInfo.cs
@@ -13,12 +13,12 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.CompiledRazorAssemblyApplicationPartFac" +
     "tory, Microsoft.AspNetCore.Mvc.Razor")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCoreApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Whatever")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyProductAttribute("DotNetCoreApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Whatever")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DotNetCoreApp.Views")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Whatever.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
